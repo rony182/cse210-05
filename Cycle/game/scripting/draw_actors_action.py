36c682fb7b78e294a68d5eec98eb26749b164163
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-03-09 23:45:01 -0300

    fixed for two snakes

--- Cycle/game/scripting/draw_actors_action.py
+++ Cycle/game/scripting/draw_actors_action.py
@@ -19,22 +19,21 @@ class DrawActorsAction(Action):
         """
         self._video_service = video_service
 
-    def execute(self, cast, script):
+    def execute(self, cast):
         """Executes the draw actors action.
 
         Args:
             cast (Cast): The cast of Actors in the game.
-            script (Script): The script of Actions in the game.
         """
         score = cast.get_first_actor("scores")
-        food = cast.get_first_actor("foods")
-        snake = cast.get_first_actor("snakes")
-        segments = snake.get_segments()
+        snakes = cast.get_actors("snakes")
         messages = cast.get_actors("messages")
 
         self._video_service.clear_buffer()
-        self._video_service.draw_actor(food)
-        self._video_service.draw_actors(segments)
+        for snake in snakes:
+            segments = snake.get_segments()
+            self._video_service.draw_actors(segments)
+        
         self._video_service.draw_actor(score)
         self._video_service.draw_actors(messages, True)
         self._video_service.flush_buffer()
