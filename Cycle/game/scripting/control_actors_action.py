42f645706c2c965f7539eda3e85ac6fdd3a5c0c4
kwazeme <ogu21006@byui.edu>
2022-03-11 16:00:46 +0200

    Add controlActorsActions2 for player2

--- Cycle/game/scripting/control_actors_action.py
+++ Cycle/game/scripting/control_actors_action.py
@@ -47,3 +47,41 @@ class ControlActorsAction(Action):
         
         snake = cast.get_first_actor("snakes")
         snake.turn_head(self._direction)
+        
+# Create keyboard actions for snake2 class
+class ControlActorsAction2(ControlActorsAction):
+    def __init__(self, keyboard_service):
+        """Constructs a second ControlActorsAction using the specified KeyboardService.
+        
+        Args:
+            keyboard_service (KeyboardService): An instance of KeyboardService.
+        """
+        super().__init__(keyboard_service)
+        self._keyboard_service = keyboard_service
+        self._direction = Point(constants.CELL_SIZE, 0)
+
+    def execute(self, cast, script):
+        """Executes the control actors action.
+
+        Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game.
+        """
+        # left
+        if self._keyboard_service.is_key_down('j'):
+            self._direction = Point(-constants.CELL_SIZE, 0)
+
+        # right
+        if self._keyboard_service.is_key_down('l'):
+            self._direction = Point(constants.CELL_SIZE, 0)
+
+        # up
+        if self._keyboard_service.is_key_down('i'):
+            self._direction = Point(0, -constants.CELL_SIZE)
+
+        # down
+        if self._keyboard_service.is_key_down('k'):
+            self._direction = Point(0, constants.CELL_SIZE)
+
+        snake2 = cast.get_second_actor("snakes")
+        snake2.turn_head(self._direction)
