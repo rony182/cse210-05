8b562baae041ed31b038a3086031065e56529e63
kwazeme <ogu21006@byui.edu>
2022-03-11 15:55:00 +0200

    Add snake2, score2 & controlActorsAction2 to main

--- Cycle/__main__.py
+++ Cycle/__main__.py
@@ -1,28 +1,30 @@
 import constants
 
 from game.casting.cast import Cast
-
+from game.casting.food import Food
 from game.casting.score import Score
+from game.casting.score import Score2
 from game.casting.snake import Snake
+from game.casting.snake import Snake2
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
+from game.scripting.control_actors_action import ControlActorsAction2
 from game.scripting.move_actors_action import MoveActorsAction
 from game.scripting.handle_collisions_action import HandleCollisionsAction
 from game.scripting.draw_actors_action import DrawActorsAction
 from game.directing.director import Director
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
-from game.shared.color import Color
-from game.shared.point import Point
-
 
 def main():
     
     # create the cast
     cast = Cast()
-    
-    cast.add_actor("Cycle", Snake())
+    cast.add_actor("foods", Food())
+    cast.add_actor("snakes", Snake())
     cast.add_actor("scores", Score())
+    cast.add_actor("snakes", Snake2())
+    cast.add_actor("scores", Score2())
    
     # start the game
     keyboard_service = KeyboardService()
@@ -30,6 +32,7 @@ def main():
 
     script = Script()
     script.add_action("input", ControlActorsAction(keyboard_service))
+    script.add_action("input",ControlActorsAction2(keyboard_service))
     script.add_action("update", MoveActorsAction())
     script.add_action("update", HandleCollisionsAction())
     script.add_action("output", DrawActorsAction(video_service))
