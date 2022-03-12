6396bd4a4032d0766d53a92842b7b57e91eb083b
BelChaparro <73005316+BelChaparro@users.noreply.github.com>
2022-03-12 20:09:10 -0300

    removed all Food class references

--- Cycle/__main__.py
+++ Cycle/__main__.py
@@ -1,18 +1,18 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.food import Food
 from game.casting.score import Score
 from game.casting.score import Score2
 from game.casting.snake import Snake
 
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
-
 from game.scripting.move_actors_action import MoveActorsAction
 from game.scripting.handle_collisions_action import HandleCollisionsAction
 from game.scripting.draw_actors_action import DrawActorsAction
+
 from game.directing.director import Director
+
 from game.services.keyboard_service import KeyboardService
 from game.services.video_service import VideoService
 
