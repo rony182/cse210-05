d816a74013fabfbe38be5cbf87d9a27fc4422dbc
BelChaparro <a.belenchaparro@gmail.com>
2022-03-12 20:53:41 -0300

    changed "snake" for "cycle"

--- Cycle/game/casting/cycle.py
+++ Cycle/game/casting/cycle.py
@@ -58,7 +58,7 @@ class Cycle(Actor):
         x = int(constants.MAX_X / 2)
         y = int(constants.MAX_Y / 2)
 
-        for i in range(constants.SNAKE_LENGTH):
+        for i in range(constants.CYCLE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "5" if i == 0 else "#"
