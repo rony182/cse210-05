249c3675d986434fa68a5ac1effa79f64ab7467d
BelChaparro <73005316+BelChaparro@users.noreply.github.com>
2022-03-12 20:25:23 -0300

    changed "snake" for "cycle"

--- Cycle/game/scripting/control_actors_action.py
+++ Cycle/game/scripting/control_actors_action.py
@@ -5,9 +5,9 @@ from game.shared.point import Point
 
 class ControlActorsAction(Action):
     """
-    An input action that controls the snake.
+    An input action that controls the cycle.
     
-    The responsibility of ControlActorsAction is to get the direction and move the snake's head.
+    The responsibility of ControlActorsAction is to get the direction and move the cycle's head.
 
     Attributes:
         _keyboard_service (KeyboardService): An instance of KeyboardService.
